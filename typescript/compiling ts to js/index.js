bfa4f5a966e7040b2dac9788b3b22fcc9052949d
nairoMudumane <nazirmudumane@gmail.com>
2021-11-25 15:49:15 +0200

    first commit

---
+++ typescript/compiling ts to js/index.js
@@ -0,0 +1,19 @@
+/** Compilar de ts para js
+ * primerio instalar o ts atraves do npm (npm install -g typescript)
+ * depois roda o comando tsc nomeArquivo.ts -w (a flag w serve para automatizar a compilacao a cada vez que salvar o arquivo js)
+ */
+var frase = 'Ola Mundo';
+console.log(frase);
+/** Variaveis implicitas
+ * Aquelas que nao se podem mudar o seu tipo
+ * a mesma logica aplica-se aos arrays e objectos
+ */
+// let nome = 'nairo';
+// errado
+// nome = 32;
+// let pessoa = {
+//     nome: 'Nairo Mudumane',
+//     idade: 32
+// }
+// // errado
+// pessoa.idade = '32';
