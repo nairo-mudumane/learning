79355d4684bb76f347e81238522c03bf15ba8712
nairoMudumane <nazirmudumane@gmail.com>
2021-11-26 16:03:35 +0200

    inputs

--- python/pythonBasico/entrada-e-saida.py
+++ python/pythonBasico/entrada-e-saida.py
@@ -5,15 +5,22 @@ print('secound print.')
 print('frist print.\nsecound print.')
 
 
-# variables
+# variables and types
 
-name = 'Nairo Mudumane'
-age = 21
-height = 1.70
+# name = 'Nairo Mudumane'
+# age = 21
+# height = 1.70
+# print(name)
+# print(type(name))
+# print(age)
+# print(type(age))
+# print(height)
+# print(type(height))
 
-print(name)
-print(type(name))
-print(age)
-print(type(age))
-print(height)
-print(type(height))
+
+# inputs
+
+name = input('Digite o seu nome\n')
+age = input('Digite a sua idade\n')
+print(name, 'tem', age, 'anos de vida.')
+print(type(name), type(age))
