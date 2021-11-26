fd8a8989183b5cac6d380496202748217cef6e40
nairoMudumane <nazirmudumane@gmail.com>
2021-11-26 15:54:33 +0200

    variables and types

--- python/pythonBasico/entrada-e-saida.py
+++ python/pythonBasico/entrada-e-saida.py
@@ -3,3 +3,17 @@ print('secound print.')
 
 # or
 print('frist print.\nsecound print.')
+
+
+# variables
+
+name = 'Nairo Mudumane'
+age = 21
+height = 1.70
+
+print(name)
+print(type(name))
+print(age)
+print(type(age))
+print(height)
+print(type(height))
