241dfa8822888d3bcc0d21d39960ed3cbe7453c8
nairoMudumane <nazirmudumane@gmail.com>
2021-11-27 11:49:42 +0200

    using quotes

---
+++ python/game_over.py
@@ -0,0 +1,71 @@
+# Game Over
+# Demonstrate the use of quotes in strings
+
+
+print("Program 'Game Over' 1.0")
+print("Same","message","as before")
+
+print("Just",
+      "a bti",
+      "bigger")
+
+print("Here", end=" ")
+print("it is...")
+
+print(
+    """
+_____
+/
+___
+___|
+/
+| |
+| |
+/ /
+| |
+_____
+/ /
+__|
+| | | |___
+|_| |_____|
+_____
+/ / |
+/ /
+___|
+/| | | |__
+/ / |__/ | | |
+_
+\ | |
+_____
+| |
+|_| /_/
+_
+| | | | | |
+___
+|/
+/ /|
+/ ___ |
+\_____/ /_/
+_
+/
+/ /| |
+_
+| |_| |
+/
+___
+|
+_____
+___| |
+_
+\
+| |__ | |_| |
+| | | | | | / / | |
+| |_| | | |/ / | |___ \_____/ |___/ |_____| |_|
+__|
+_
+/
+| | \ \
+\_\
+"""
+    )
+input("\n\nPress the enter key to exit.")
